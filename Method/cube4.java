59d30faef7ad973aa25446252692a5fdcd0fbd5f
Prashant Srivastava <129353763+PrashantSoftSolution@users.noreply.github.com>
2024-10-09 10:00:48 -0700

    Add files via upload

---
+++ Method/cube4.java
@@ -0,0 +1,11 @@
+// with parameter without return type
+
+public class cube4 {
+    static void cube(int n){
+        int res=n*n*n;
+        System.out.print(res);
+    }
+    public static void main(String[] args) {
+        cube(10);
+    }
+}
