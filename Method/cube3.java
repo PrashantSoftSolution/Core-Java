fa16376669ffc64fda16685420b639c7ed23ae3c
Prashant Srivastava <129353763+PrashantSoftSolution@users.noreply.github.com>
2024-10-09 10:00:04 -0700

    Add files via upload

---
+++ Method/cube3.java
@@ -0,0 +1,13 @@
+// with parameter with return type
+
+public class cube3 {
+    static int cubenum(int n){
+        int res= n*n*n;
+        return res;
+    }
+    public static void main(String[] args) {
+        int a= cubenum(2);
+        System.out.print(a);
+    }
+    
+}
