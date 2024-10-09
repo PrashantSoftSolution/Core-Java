91a8a7c07e09cf08a23086f8f790859330ea7070
Prashant Srivastava <129353763+PrashantSoftSolution@users.noreply.github.com>
2024-10-09 09:56:07 -0700

    add new file

---
+++ Method/Cube1.java
@@ -0,0 +1,13 @@
+// without parameter without return type
+
+public class cube1 {
+    static void cubenum(){
+        int n= 3;
+        int res = n*n*n;
+        System.out.print(res);
+    }
+    public static void main(String[] args) {
+        cubenum();
+    }
+    
+}
